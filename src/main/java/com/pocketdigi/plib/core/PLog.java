56ed7c7e6dd8b8047d97f68ceb26e4e452ab70ff
方海鹏 <wb-fanghaipeng@alibaba-inc.com>
2014-10-27 17:07:43 +0800

    修复日志不输出bug

--- src/main/java/com/pocketdigi/plib/core/PLog.java
+++ src/main/java/com/pocketdigi/plib/core/PLog.java
@@ -8,50 +8,53 @@ import com.pocketdigi.plib.BuildConfig;
  * Created by fhp on 14-9-2.
  */
 public class PLog {
+    //在Android Studio里，Library项目一定是release发布的，所以不能再用BuildConfig.DEBUG判断
+    //需要在application里的onCreate里，加 PLog.DEBUG=BuildConfig.DEBUG
+    public static boolean DEBUG=false;
     public static int v(String tag, String msg) {
-        if (BuildConfig.DEBUG)
+        if (DEBUG)
             return Log.v(tag, msg);
         return 0;
     }
 
     public static int v(String tag, String msg, Throwable tr) {
-        if (BuildConfig.DEBUG)
+        if (DEBUG)
             return Log.v(tag, msg, tr);
         return 0;
     }
 
     public static int d(String tag, String msg) {
-        if (BuildConfig.DEBUG)
+        if (DEBUG)
             return Log.d(tag, msg);
         return 0;
     }
 
     public static int d(String tag, String msg, Throwable tr) {
-        if (BuildConfig.DEBUG)
+        if (DEBUG)
             return Log.d(tag, msg, tr);
         return 0;
     }
 
     public static int i(String tag, String msg) {
-        if (BuildConfig.DEBUG)
+        if (DEBUG)
             return Log.i(tag, msg);
         return 0;
     }
 
     public static int i(String tag, String msg, Throwable tr) {
-        if (BuildConfig.DEBUG)
+        if (DEBUG)
             return Log.i(tag, msg, tr);
         return 0;
     }
 
     public static int w(String tag, String msg) {
-        if (BuildConfig.DEBUG)
+        if (DEBUG)
             return Log.w(tag, msg);
         return 0;
     }
 
     public static int w(String tag, String msg, Throwable tr) {
-        if (BuildConfig.DEBUG)
+        if (DEBUG)
             return Log.w(tag, msg,tr);
         return 0;
     }
@@ -61,50 +64,50 @@ public class PLog {
     }
 
     public static int w(String tag, Throwable tr) {
-        if (BuildConfig.DEBUG)
+        if (DEBUG)
             return Log.w(tag,tr);
         return 0;
     }
 
     public static int e(String tag, String msg) {
-        if (BuildConfig.DEBUG)
+        if (DEBUG)
             return Log.e(tag,msg);
         return 0;
     }
 
     public static int e(String tag, String msg, Throwable tr) {
-        if (BuildConfig.DEBUG)
+        if (DEBUG)
             return Log.e(tag,msg,tr);
         return 0;
     }
 
     public static int e(Object obj,String msg)
     {
-        if (BuildConfig.DEBUG)
+        if (DEBUG)
             return Log.e(obj.getClass().getSimpleName(),msg);
         return 0;
     }
     public static int d(Object obj,String msg)
     {
-        if (BuildConfig.DEBUG)
+        if (DEBUG)
             return Log.d(obj.getClass().getSimpleName(),msg);
         return 0;
     }
     public static int v(Object obj,String msg)
     {
-        if (BuildConfig.DEBUG)
+        if (DEBUG)
             return Log.v(obj.getClass().getSimpleName(),msg);
         return 0;
     }
     public static int w(Object obj,String msg)
     {
-        if (BuildConfig.DEBUG)
+        if (DEBUG)
             return Log.w(obj.getClass().getSimpleName(),msg);
         return 0;
     }
     public static int d(Object obj,Object msgObj)
     {
-        if (BuildConfig.DEBUG)
+        if (DEBUG)
             return Log.d(obj.getClass().getSimpleName(),msgObj.toString());
         return 0;
     }
