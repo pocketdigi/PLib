8496d648f54afe501c116c42776e0778b15dfd3a
exception <webmaster@pocketdigi.com>
2015-02-13 10:03:02 +0800

    修复日期周转换错误

--- src/main/java/com/pocketdigi/plib/util/DateUtils.java
+++ src/main/java/com/pocketdigi/plib/util/DateUtils.java
@@ -111,7 +111,7 @@ public class DateUtils {
     public static String getChineseWeekDay(long timestamp,int mode) {
         Calendar calendar=Calendar.getInstance();
         calendar.setTimeInMillis(timestamp);
-        String[] days={"周","一","二","三","四","五","六"};
+        String[] days={"日","一","二","三","四","五","六"};
         int dayOfWeak = calendar.get(Calendar.DAY_OF_WEEK);
         switch (mode){
             case 0:
