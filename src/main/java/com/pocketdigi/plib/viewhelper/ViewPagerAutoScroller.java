933b5e4257561a2da249bb28d90454cff96b268f
fhp <webmaster@pocketdigi.com>
2015-07-17 09:36:38 +0800

    消息中心，工单详情完成页，安全组件

--- src/main/java/com/pocketdigi/plib/viewhelper/ViewPagerAutoScroller.java
+++ src/main/java/com/pocketdigi/plib/viewhelper/ViewPagerAutoScroller.java
@@ -13,49 +13,50 @@ import java.util.List;
  * ViewPager自动轮播,Adapter的getCount必须返回Integer.MAX_VALUE
  * Created by fhp on 15/7/16.
  */
-public class ViewPagerAutoScroller implements ViewPager.OnPageChangeListener{
+public class ViewPagerAutoScroller implements ViewPager.OnPageChangeListener {
     List<ViewPager.OnPageChangeListener> onPageChangeListenerList;
     //动画周期
-    int period=5000;
+    int period = 5000;
     ViewPager viewPager;
     Handler handler;
+
     public ViewPagerAutoScroller(ViewPager viewPager) {
-        this.viewPager=viewPager;
-        onPageChangeListenerList=new ArrayList<>();
-        handler=new Handler(Looper.getMainLooper());
+        this.viewPager = viewPager;
+        onPageChangeListenerList = new ArrayList<>();
+        handler = new Handler(Looper.getMainLooper());
     }
 
     /**
      * 设置播放周期，
+     *
      * @param period ms
      */
     public void setPeriod(int period) {
         this.period = period;
     }
+
     /**
      * 自动轮播
      */
     public void autoScroll() {
-        if(viewPager.getAdapter().getCount()==Integer.MAX_VALUE) {
-            viewPager.setOnPageChangeListener(this);
-            handler.postDelayed(autoScroll, period);
-        }else {
-            PToast.show("Adapter返回Count必须是Integer.MAX_VALUE才能支持轮播");
-        }
-
+        viewPager.setOnPageChangeListener(this);
+        handler.postDelayed(autoScroll, period);
     }
+
     public void stopScroll() {
         handler.removeCallbacks(autoScroll);
     }
-    public void destory(){
+
+    public void destory() {
         stopScroll();
         removeAllPageChangeListener();
-        viewPager=null;
+        viewPager = null;
     }
 
     public void addOnPageChangeListener(ViewPager.OnPageChangeListener onPageChangeListener) {
         onPageChangeListenerList.add(onPageChangeListener);
     }
+
     public void removeOnPageChangeListener(ViewPager.OnPageChangeListener onPageChangeListener) {
         onPageChangeListenerList.remove(onPageChangeListener);
     }
@@ -69,31 +70,37 @@ public class ViewPagerAutoScroller implements ViewPager.OnPageChangeListener{
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-        for(ViewPager.OnPageChangeListener onPageChangeListener:onPageChangeListenerList) {
-            onPageChangeListener.onPageScrolled(position,positionOffset,positionOffsetPixels);
+        for (ViewPager.OnPageChangeListener onPageChangeListener : onPageChangeListenerList) {
+            onPageChangeListener.onPageScrolled(position, positionOffset, positionOffsetPixels);
         }
     }
 
     @Override
     public void onPageSelected(int position) {
-        for(ViewPager.OnPageChangeListener onPageChangeListener:onPageChangeListenerList) {
+        for (ViewPager.OnPageChangeListener onPageChangeListener : onPageChangeListenerList) {
             onPageChangeListener.onPageSelected(position);
         }
         handler.removeCallbacks(autoScroll);
-        handler.postDelayed(autoScroll,period);
+        handler.postDelayed(autoScroll, period);
     }
 
     @Override
     public void onPageScrollStateChanged(int state) {
-        for(ViewPager.OnPageChangeListener onPageChangeListener:onPageChangeListenerList) {
+        for (ViewPager.OnPageChangeListener onPageChangeListener : onPageChangeListenerList) {
             onPageChangeListener.onPageScrollStateChanged(state);
         }
     }
 
-    Runnable autoScroll=new Runnable() {
+    Runnable autoScroll = new Runnable() {
         @Override
         public void run() {
-            viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+            int count = viewPager.getAdapter().getCount();
+            int currentItem = viewPager.getCurrentItem();
+            int target=0;
+            if(currentItem<count-1) {
+                target=currentItem+1;
+            }
+            viewPager.setCurrentItem(target);
         }
     };
 
