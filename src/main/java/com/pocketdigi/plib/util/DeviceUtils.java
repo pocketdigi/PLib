cae351c9babf6f36818af4031a3c527c57d988bf
fhp <webmaster@pocketdigi.com>
2015-08-04 18:19:17 +0800

    bug fix

--- src/main/java/com/pocketdigi/plib/util/DeviceUtils.java
+++ src/main/java/com/pocketdigi/plib/util/DeviceUtils.java
@@ -6,6 +6,7 @@ import android.graphics.Rect;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
@@ -106,5 +107,9 @@ public class DeviceUtils {
     public static void showSoftInput(View view) {
         InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.showSoftInput(view, 0);
+    }
+
+    public static int convertDpToPx(float dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, PApplication.getInstance().getResources().getDisplayMetrics());
     }
 }
